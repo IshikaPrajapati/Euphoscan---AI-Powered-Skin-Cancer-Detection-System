a79fc6e9499e99342fcb8d2a4fd2969675edaa0f
Ishika Prajapati <art.ishi.03@gmail.com>
2026-06-20 17:51:20 +0530

    Fix SavedModel loading

--- app.py
+++ app.py
@@ -409,12 +409,227 @@
 # )
 
 
+#>>>>>>>>>>>>>>>>>>>>>>>>>
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+
+# from flask import Flask, render_template, request
+# import os
+# import numpy as np
+# from PIL import Image
+# from tensorflow.keras.models import load_model
+# import tensorflow as tf
+# import keras
+
+# app = Flask(__name__)
+
+# # ==================================
+# # Model Path
+# # ==================================
+
+# # MODEL_PATH = "model/euphoscan_model.keras"
+# # MODEL_PATH = "model/euphoscan_render.keras"
+# MODEL_PATH = "saved_model"
+# model = tf.keras.models.load_model(MODEL_PATH)
+# # ==================================
+# # Render Debug Information
+# # ==================================
+
+# print("=" * 50)
+# print("TensorFlow Version:", tf.__version__)
+# print("Keras Version:", keras.__version__)
+# print("Current Directory:", os.getcwd())
+# print("Model Exists:", os.path.exists(MODEL_PATH))
+
+# if os.path.exists(MODEL_PATH):
+#     print("Model Size:", os.path.getsize(MODEL_PATH))
+
+# print("=" * 50)
+
+# # ==================================
+# # Load Model
+# # ==================================
+
+# print("Loading model...")
+
+# model = load_model(
+#     MODEL_PATH,
+#     compile=False
+# )
+
+# print("Model loaded successfully!")
+# print("Model input shape:", model.input_shape)
+
+# # ==================================
+# # Class Names
+# # ==================================
+
+# class_names = [
+#     "Actinic Keratoses",
+#     "Basal Cell Carcinoma",
+#     "Benign Keratosis",
+#     "Dermatofibroma",
+#     "Melanoma",
+#     "Nevus",
+#     "Vascular Lesions"
+# ]
+
+# # ==================================
+# # Routes
+# # ==================================
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+# @app.route("/howItWorks")
+# def how_it_works():
+#     return render_template("howItWorks.html")
+
+
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
+
+
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html")
+
+
+# @app.route("/predict", methods=["GET", "POST"])
+# def predict():
+
+#     if request.method == "POST":
+
+#         if "file" not in request.files:
+#             return "No file uploaded", 400
+
+#         file = request.files["file"]
+
+#         if file.filename == "":
+#             return "No selected file", 400
+
+#         try:
+
+#             # Create uploads folder
+#             upload_folder = "uploads"
+#             os.makedirs(upload_folder, exist_ok=True)
+
+#             # Save uploaded image
+#             filepath = os.path.join(upload_folder, file.filename)
+#             file.save(filepath)
+
+#             # ==================================
+#             # Image Preprocessing
+#             # ==================================
+
+#             img = Image.open(filepath).convert("RGB")
+
+#             # MobileNetV2 input size
+#             img = img.resize((224, 224))
+
+#             img_array = np.array(img).astype(np.float32)
+
+#             # Normalize
+#             img_array = img_array / 255.0
+
+#             # Add batch dimension
+#             img_array = np.expand_dims(img_array, axis=0)
+
+#             print("=" * 50)
+#             print("MODEL EXPECTS :", model.input_shape)
+#             print("IMAGE SENT    :", img_array.shape)
+#             print("=" * 50)
+
+#             # ==================================
+#             # Prediction
+#             # ==================================
+
+#             prediction = model.predict(img_array)
+
+#             predicted_class = int(np.argmax(prediction))
+#             confidence = float(np.max(prediction) * 100)
+
+#             confidence_list = []
+
+#             for i in range(len(class_names)):
+#                 confidence_list.append(
+#                     (
+#                         class_names[i],
+#                         float(prediction[0][i] * 100)
+#                     )
+#                 )
+
+#             confidence_list.sort(
+#                 key=lambda x: x[1],
+#                 reverse=True
+#             )
+
+#             label = class_names[predicted_class]
+
+#             return render_template(
+#                 "predict.html",
+#                 label=label,
+#                 confidence=round(confidence, 2),
+#                 confidence_list=confidence_list
+#             )
+
+#         except Exception as e:
+#             print("PREDICTION ERROR:")
+#             print(str(e))
+#             return f"Error processing image: {str(e)}", 500
+
+#     return render_template("predict.html")
+# @app.route("/debug-model")
+# def debug_model_route():
+#     import hashlib
+
+#     model_path = "model/euphoscan_model.keras"
+
+#     if not os.path.exists(model_path):
+#         return {"error": "Model not found"}
+
+#     sha256 = hashlib.sha256()
+
+#     with open(model_path, "rb") as f:
+#         while chunk := f.read(8192):
+#             sha256.update(chunk)
+
+#     return {
+#         "exists": True,
+#         "size": os.path.getsize(model_path),
+#         "sha256": sha256.hexdigest()
+#     }
+
+# # ==================================
+# # Run Flask
+# # ==================================
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+
+#     app.run(
+#         host="0.0.0.0",
+#         port=port
+#     )
+
+
+
+#?????????????????????????????????
+#???????????????????????????????????????
+#?????????????????????????????????????????????
 
 from flask import Flask, render_template, request
 import os
 import numpy as np
 from PIL import Image
-from tensorflow.keras.models import load_model
 import tensorflow as tf
 import keras
 
@@ -424,10 +639,8 @@ app = Flask(__name__)
 # Model Path
 # ==================================
 
-# MODEL_PATH = "model/euphoscan_model.keras"
-# MODEL_PATH = "model/euphoscan_render.keras"
 MODEL_PATH = "saved_model"
-model = tf.keras.models.load_model(MODEL_PATH)
+
 # ==================================
 # Render Debug Information
 # ==================================
@@ -437,25 +650,20 @@ print("TensorFlow Version:", tf.__version__)
 print("Keras Version:", keras.__version__)
 print("Current Directory:", os.getcwd())
 print("Model Exists:", os.path.exists(MODEL_PATH))
-
-if os.path.exists(MODEL_PATH):
-    print("Model Size:", os.path.getsize(MODEL_PATH))
-
 print("=" * 50)
 
 # ==================================
-# Load Model
+# Load SavedModel
 # ==================================
 
 print("Loading model...")
 
-model = load_model(
-    MODEL_PATH,
-    compile=False
-)
+model = tf.saved_model.load(MODEL_PATH)
+
+infer = model.signatures["serving_default"]
 
 print("Model loaded successfully!")
-print("Model input shape:", model.input_shape)
+print("Available Signatures:", list(model.signatures.keys()))
 
 # ==================================
 # Class Names
@@ -510,41 +718,36 @@ def predict():
 
         try:
 
-            # Create uploads folder
             upload_folder = "uploads"
             os.makedirs(upload_folder, exist_ok=True)
 
-            # Save uploaded image
             filepath = os.path.join(upload_folder, file.filename)
             file.save(filepath)
 
-            # ==================================
+            # ==========================
             # Image Preprocessing
-            # ==================================
+            # ==========================
 
             img = Image.open(filepath).convert("RGB")
-
-            # MobileNetV2 input size
             img = img.resize((224, 224))
 
             img_array = np.array(img).astype(np.float32)
-
-            # Normalize
             img_array = img_array / 255.0
-
-            # Add batch dimension
             img_array = np.expand_dims(img_array, axis=0)
 
             print("=" * 50)
-            print("MODEL EXPECTS :", model.input_shape)
-            print("IMAGE SENT    :", img_array.shape)
+            print("IMAGE SHAPE:", img_array.shape)
             print("=" * 50)
 
-            # ==================================
+            # ==========================
             # Prediction
-            # ==================================
+            # ==========================
 
-            prediction = model.predict(img_array)
+            input_tensor = tf.convert_to_tensor(img_array)
+
+            outputs = infer(input_tensor)
+
+            prediction = list(outputs.values())[0].numpy()
 
             predicted_class = int(np.argmax(prediction))
             confidence = float(np.max(prediction) * 100)
@@ -579,26 +782,16 @@ def predict():
             return f"Error processing image: {str(e)}", 500
 
     return render_template("predict.html")
+
+
 @app.route("/debug-model")
 def debug_model_route():
-    import hashlib
-
-    model_path = "model/euphoscan_model.keras"
-
-    if not os.path.exists(model_path):
-        return {"error": "Model not found"}
-
-    sha256 = hashlib.sha256()
-
-    with open(model_path, "rb") as f:
-        while chunk := f.read(8192):
-            sha256.update(chunk)
-
     return {
-        "exists": True,
-        "size": os.path.getsize(model_path),
-        "sha256": sha256.hexdigest()
+        "model_exists": os.path.exists(MODEL_PATH),
+        "tensorflow_version": tf.__version__,
+        "cwd": os.getcwd()
     }
+
 
 # ==================================
 # Run Flask
